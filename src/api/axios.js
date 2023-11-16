825b989f1134d5864f33fef7acc28ca98b9313d2
unknown <sheikh.ameen252@gmail.com>
2023-11-17 03:13:40 +0500

    changed interceptor code

--- src/api/axios.js
+++ src/api/axios.js
@@ -9,13 +9,12 @@ const ResponseInterceptor = (response) => {
 };
 
 const RequestInterceptor = (config) => {
-  let token = JSON.parse(localStorage.getItem("token"));
-  console.log("tok", "Bearer " + token.trim())
-  if (token) {
-    config.headers.Authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGYzYzM2M2M5NTYzNTNhNzhiZWQyMzMiLCJpYXQiOjE3MDAxNjA0ODcsImV4cCI6MTcwMDc2NTI4N30.NY8MS1Fyb7w9QjfY_5f8fsSFL4E-GAwHsIecftXGG60"
-    config.headers.Accept = "application/json";
-  }
-  return config;
+    let token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = "Bearer " + token;
+      config.headers.Accept = "application/json";
+    }
+    return config;
 };
 
 axiosInstance.interceptors.response.use(ResponseInterceptor, async (error) => {
